1ea98619292ec23f5a8542d2b0a69f89557c5b11
Nicole612 <nicolemedy666@gmail.com>
2025-05-07 22:06:01 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,16 +10,22 @@ def load_data(file_path):
 def generate_animals_data(animals_data):
     html_animals_infos = ""
     for animal in animals_data:
-        html_animals_infos += "<li class='cards_item'>\n"
-        if animal.get('name'):
-            html_animals_infos += f"Name: {animal['name']}<br>\n"
-        if animal['characteristics'].get('order'):
-            html_animals_infos += f"Diet: {animal['characteristics']['order']}<br>\n"
-        if animal.get('locations'):
-            html_animals_infos += f"Location: {animal['locations']}<br>\n"
-        if animal['characteristics'].get('type'):
-            html_animals_infos += f"Type: {animal['characteristics']['type']}<br>\n"
-        html_animals_infos += "<br><li>\n"
+        html_animals_infos += "<li class='cards__item'>"
+        if animal.get("name"):
+            animal_name = animal["name"]
+            html_animals_infos += f"<div class='card__title'>{animal_name}</div>"
+        html_animals_infos += "<p class='card__text'>"
+        if animal["characteristics"].get("diet"):
+            animal_diet = animal["characteristics"]["diet"]
+            html_animals_infos += f"    <strong>Diet: </strong>{animal_diet}<br>"
+        if animal.get("locations"):
+            animal_locations = animal["locations"][0]
+            html_animals_infos += f"    <strong>Location: </strong>{animal_locations}<br>\n"
+        if animal["characteristics"].get("type"):
+            animal_type = animal["characteristics"]["type"]
+            html_animals_infos += f"    <strong>Type: </strong>{animal_type}<br>"
+        html_animals_infos += "</p>"
+        html_animals_infos += "<li>"
     return html_animals_infos
 
 def main():
