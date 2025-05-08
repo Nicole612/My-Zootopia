0fe8822f835c40ca305c07a335145c8b688dbede
Nicole612 <nicolemedy666@gmail.com>
2025-05-08 15:11:06 +0200

    feat: fetch animal data from API instead of local JSON (default: Fox)

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,3 +1,4 @@
+import requests
 import json
 from tempfile import template
 
@@ -8,6 +9,7 @@ def load_data(file_path):
     return json.load(handle)
 
 def serialize_animal(animal):
+    """Generates HTML for a single animal as a <li> element"""
     animal_data = "<li class='cards__item'>"
     if animal.get("name"):
         animal_data += f"<div class='card__title'>{animal["name"]}</div>"
@@ -24,13 +26,16 @@ def serialize_animal(animal):
 
 
 def generate_animals_data(animals_data):
+    """ Generates the complete HTML string for a list of animals."""
     html_animals_infos = ""
     for animal in animals_data:
         html_animals_infos += serialize_animal(animal)
     return html_animals_infos
 
 def main():
-    animals_data = load_data('animals_data.json')
+    API_KEY = "0mRBFkXQco3Snim/4O343Q==qRv3QTRm0jPEkVg6"
+    res = requests.get(f"https://api.api-ninjas.com/v1/animals?name=Fox", headers={"X-Api-Key": API_KEY})
+    animals_data = res.json()
     with open("animals_template.html", "r") as file:
         template_html = file.read()
     updated_html_file = template_html.replace("__REPLACE_ANIMALS_INFO__", generate_animals_data(animals_data) )
